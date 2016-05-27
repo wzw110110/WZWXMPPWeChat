69c9b2d137f48ae11f08477f349da3bb06f9bf79
iOS <www.pusai.net@wuzhiweidemac-mini.local>
2016-05-27 09:05:59 +0800

    Initial Commit

---
+++ WZWXMPPWeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WZWXMPPWeChat
+//
+//  Created by iOS on 16/5/27.
+//  Copyright © 2016年 wzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WZWXMPPWeChat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WZWXMPPWeChat
+//
+//  Created by iOS on 16/5/27.
+//  Copyright © 2016年 wzw. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
