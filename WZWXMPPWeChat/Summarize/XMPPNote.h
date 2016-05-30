d86858972335a4a4302b6464a3da6e709618f2eb
iOS <www.pusai.net@wuzhiweidemac-mini.local>
2016-05-30 10:00:52 +0800

    xmpp框架的导入及配置

---
+++ WZWXMPPWeChat/Summarize/XMPPNote.h
@@ -0,0 +1,21 @@
+//
+//  XMPPNote.h
+//  WZWXMPPWeChat
+//
+//  Created by iOS on 16/5/27.
+//  Copyright © 2016年 wzw. All rights reserved.
+//
+
+#ifndef XMPPNote_h
+#define XMPPNote_h
+
+/*
+  XMPP框架的导入
+  选择Building Phases ->Linked Framework and Libraries添加对应的依赖
+   1、添加libxml2，同时在Building Settings中搜索 Other Linker Flags添加
+    -lxml2，搜索Header Search，在Header Search Paths中添加/usr/include/libxml2
+   2、添加libresolv
+   3、将sample_xmppFramework.h改成xmppFramework.h
+ */
+
+#endif
